006098e343f5c39d623ef3b2d4ad93dbdb8527e8
RB <87038824+MrMhirs@users.noreply.github.com>
2022-05-23 11:46:41 +0200

    Starting Backend

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use("/posts", postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const URL =
+  "mongodb+srv://BadCoder:BadCoder@cluster0.1vcih.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect(URL)
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} Server did not connect`));
